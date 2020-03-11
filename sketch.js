34fddf4382dc3f02576e41ef9f39b789ac48d331
maliyah907 <51923749+maliyah907@users.noreply.github.com>
2020-03-11 10:57:10 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,33 +1,56 @@
-# maliyah907.github.io
-let x = 200;
-let y = 200;
-let extraCanvas;
+var spot = {
+    x: 100,
+    y: 50
+}
+
+var col = {
+    r: 255,
+    g: 0,
+    b: 0
+}
 
 function setup() {
-  createCanvas(400, 400);
-  extraCanvas = createGraphics(400, 400);
-  extraCanvas.clear();
-  background(0);
+    createCanvas(600, 400);
+    background(0);
 }
 
 function draw() {
-  background(0);
-  x += random(-5, 5);
-  y += random(-5, 5);
-  
-  //Trails
-  //if (mouseIsPressed) {
-  extraCanvas.fill(255, 150);
-  extraCanvas.noStroke();
-  let starX = random(width);
-  let starY = random(height);
-  extraCanvas.ellipse(starX, starY, 10, 10);
-  
-  //extraCanvas.ellipse(mouseX, mouseY, 60, 60);
-//}
-image(extraCanvas, 0, 0);
-fill(255, 0, 0);
-stroke(255);
-rectMode(CENTER);
-rect(x, y, 20, 20);
+    col.r = random(100,255);
+    col.g = 0;
+    col.b = random(100,190);
+    spot.x = random(0,width);
+    spot.y = random(0,height);
+    fill(col.r, col.g, col.b,100);
+    noStroke();
+    ellipse(spot.x, spot.y, 24, 24);
+
+}
+
+{var ballX = 200.0
+    var ballY = 100.0
+    var ballSpeedY = 0.0
+
+    var gravity = 0.1;
+
+    function setup() {
+        createCanvas(400, 400);
+    }
+
+    function draw() {
+        background(150, 200, 250);
+
+        //to make the ball "fall" down, every frame we increase its speed by a tiny amount. We call this amount gravity.
+
+        ballSpeedY = ballSpeedY+gravity
+
+        ballY = ballY+ballSpeedY
+
+        fill(255, 0, 0)
+        ellipse(ballX, ballY, 20.0, 20.0)
+
+        if (ballY >= height) {
+            ballSpeedY = ballSpeedY *-.9
+        }
+    }
+
 }
